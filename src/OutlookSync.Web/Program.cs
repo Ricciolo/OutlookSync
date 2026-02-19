dadd04e8d7b8dd5f4851d48d8bafa46d6abecd89
Cristian Civera <cristian@cristiancivera.com>
2026-02-19 23:55:01 +0000

    feat: implement basic authentication and update launch settings for HTTP

--- src/OutlookSync.Web/Program.cs
+++ src/OutlookSync.Web/Program.cs
@@ -27,12 +27,9 @@ await app.ApplyMigrationsAsync();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
